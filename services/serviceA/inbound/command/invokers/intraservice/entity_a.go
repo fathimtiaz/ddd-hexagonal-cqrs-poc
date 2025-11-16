0ac620b33b33048fe1e218d32573b52e22871aac
Fath Imtiaz <fathimtiaz@gmail.com>
2025-11-16 05:14:54 +0000

    dor

--- services/serviceA/inbound/command/invokers/intraservice/entity_a.go
+++ services/serviceA/inbound/command/invokers/intraservice/entity_a.go
@@ -1,1 +1,15 @@
 package intraservice
+
+import (
+	"context"
+
+	inputCommand "ddd-hexagonal-cqrs-poc/services/serviceA/io/input/command"
+)
+
+func (i *Invoker) EntityAMove(ctx context.Context, cmd inputCommand.EntityAMoveCommand) error {
+	return i.handler.EntityAMove(ctx, cmd)
+}
+
+func (i *Invoker) EntityACreate(ctx context.Context, cmd inputCommand.EntityACreateCommand) error {
+	return i.handler.EntityACreate(ctx, cmd)
+}
--- services/serviceA/inbound/command/invokers/intraservice/invoker.go
+++ services/serviceA/inbound/command/invokers/intraservice/invoker.go
@@ -1,9 +1,7 @@
 package intraservice
 
 import (
-	"context"
 	inboundCommand "ddd-hexagonal-cqrs-poc/services/serviceA/inbound/command"
-	inputCommand "ddd-hexagonal-cqrs-poc/services/serviceA/io/input/command"
 )
 
 // Invoker is the cross-service bridge for in-process calls to ServiceA from other services.
@@ -14,12 +12,4 @@ type Invoker struct {
 // NewInvokerWithHandler constructs an inbound invoker with injected handler.
 func NewInvokerWithHandler(handler inboundCommand.Handler) *Invoker {
 	return &Invoker{handler}
-}
-
-func (i *Invoker) EntityAMove(ctx context.Context, cmd inputCommand.EntityAMoveCommand) error {
-	return i.handler.EntityAMove(ctx, cmd)
-}
-
-func (i *Invoker) EntityACreate(ctx context.Context, cmd inputCommand.EntityACreateCommand) error {
-	return i.handler.EntityACreate(ctx, cmd)
 }
